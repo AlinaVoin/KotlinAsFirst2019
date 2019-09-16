bf979601433585bea470479d45d4a68ec7f4f265
AlinaVoin <cat1247@bk.ru>
2019-09-16 13:19:39 +0300

    2 урок

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,8 +3,9 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
 import kotlin.math.max
-import kotlin.math.sqrt
+import kotlin.math.*
 
 /**
  * Пример
@@ -63,7 +64,13 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String = when {
+    (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && ((age / 10) % 10 == 1) -> "age лет"
+    (age % 10 == 1) && ((age / 10) % 10 != 1) -> "age год"
+    (age % 10 == 1) && ((age / 10) % 10 == 1) -> "age лет"
+    (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && ((age / 10) % 10 != 1) -> "age года"
+    else -> "age лет"
+}
 
 /**
  * Простая
@@ -76,7 +83,19 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val sHalf = ((t1 * v1) + (t2 * v2) + (t3 * v3)) / 2
+    val tHalf: Double
+    when {
+        (sHalf <= (t1 * v1)) -> tHalf = sHalf / v1
+        (sHalf <= t1 * v1 + t2 * v2) -> tHalf = t1 + ((sHalf - t1 * v1) / v2)
+        else -> tHalf = (t1 + t2 + (sHalf - t1 * v1 - t2 * v2) / v3)
+    }
+    return tHalf
+}
+
+
+
 
 /**
  * Простая
@@ -91,7 +110,15 @@ fun whichRookThreatens(
     kingX: Int, kingY: Int,
     rookX1: Int, rookY1: Int,
     rookX2: Int, rookY2: Int
-): Int = TODO()
+): Int = when {
+    (kingX == rookX1) && (kingX != rookX2) && (kingY != rookY2) -> 1
+    (kingY == rookY1) && (kingX != rookX2) && (kingY != rookY2) -> 1
+    (kingX == rookX2) && (kingX != rookX1) && (kingY != rookY1) -> 2
+    (kingY == rookY2) && (kingX != rookX1) && (kingY != rookY1) -> 2
+    (kingX == rookX1) && (kingX == rookX2) -> 3
+    (kingY == rookY1) && (kingY == rookY2) -> 3
+    else -> 0
+}
 
 /**
  * Простая
@@ -107,7 +134,14 @@ fun rookOrBishopThreatens(
     kingX: Int, kingY: Int,
     rookX: Int, rookY: Int,
     bishopX: Int, bishopY: Int
-): Int = TODO()
+): Int = when {
+    (kingX == rookX) && (abs(kingX - bishopX) != abs(kingY - bishopY)) -> 1
+    (kingY == rookY) && (abs(kingX - bishopX) != abs(kingY - bishopY)) -> 1
+    abs(kingX - bishopX) == abs(kingY - bishopY) && (kingX != rookX) && (kingY != rookY) -> 2
+    (kingX == rookX) && (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
+    (kingY == rookY) && (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
+    else -> 0
+}
 
 /**
  * Простая
@@ -117,7 +151,32 @@ fun rookOrBishopThreatens(
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    if ((a + b < c) && (a + c < b) && (b + c < a)) {
+        if (a > b) {
+            if (a > c) when {
+                sqr(b) + sqr(c) > sqr(a) -> 0
+                sqr(b) + sqr(c) < sqr(a) -> 2
+                else -> 1
+            }
+            else when {
+                sqr(a) + sqr(b) > sqr(c) -> 0
+                sqr(a) + sqr(b) < sqr(c) -> 2
+                else -> 1
+            }
+            else (if (b > c)) when {
+                sqr(a) + sqr(c) > sqr(b) -> 0
+                sqr(a) + sqr(c) < sqr(b) -> 2
+                else -> 1
+            }
+            else when {
+                sqr(a) + sqr(b) > sqr(c) -> 0
+                sqr(a) + sqr(b) < sqr(c) -> 2
+                else -> 1
+            }
+        }
+    } else -1
+}
 
 /**
  * Средняя
@@ -127,4 +186,10 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = when {
+    (c < b) && (a < c) && (d > b) -> b - c
+    (d > a) && (d < b) && (c < a) -> d - a
+    (c < a) && (c < b) && (d > a) && (d > b) -> b - a
+    (a < c) && (a < d) && (b > c) && (b > d) -> d - c
+    else -> -1
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.abs
 
 /**
  * Пример
@@ -18,7 +19,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean =
+    (number / 1000 + (number / 100) % 10) == (number % 10 + (number / 10) % 10)
+
 
 /**
  * Простая
@@ -27,7 +30,13 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = when {
+    ((x1 == 1) || (x1 == 8) || (y1 == 1) || (y1 == 8)) && ((x2 == 1) || (x2 == 8) || (y2 == 1) || (y2 == 8)) -> false
+    (x1 == x2) && (y1 != y2) -> true
+    (x1 != x2) && (y1 == y2) -> true
+    (abs(x1 - x2)) == (abs(y1 - y2)) -> true
+    else -> false
+}
 
 
 /**
@@ -36,7 +45,12 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int = when {
+    (month == 1 or 3 or 5 or 7 or 8 or 10 or 12) -> 31
+    (month == 4 or 6 or 9 or 11) -> 30
+    (month == 2) and (year % 4 == 0) -> 29
+    else -> 28
+}
 
 /**
  * Средняя
@@ -48,7 +62,10 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = when {
+    sqr(x2 - x1) + sqr(y2 - y1) < sqr(r2 - r1) -> true
+    else -> false
+}
 
 /**
  * Средняя
@@ -59,4 +76,7 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = when {
+    ((a <= r) and (b <= s)) or ((b <= r) and (a <= s)) or ((a <= r) and (c <= s)) or ((c <= r) and (a <= s)) or ((b <= r) and (c <= s)) or ((c <= r) and (b <= s)) -> true
+    else -> false
+}
